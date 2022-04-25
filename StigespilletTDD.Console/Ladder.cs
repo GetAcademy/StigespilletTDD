ecafbf06ba2061ff7b2a553291b7761cab2e27d3
GET Academy <terje@getacademy.no>
2022-04-25 14:37:23 +0200

    Initial commit

---
+++ StigespilletTDD.Console/Ladder.cs
@@ -0,0 +1,14 @@
+﻿namespace StigespilletTDD.Console
+{
+    internal class Ladder
+    {
+        public int PositionFrom { get; }
+        public int PositionTo { get; }
+
+        public Ladder(int positionFrom, int positionTo)
+        {
+            PositionFrom = positionFrom;
+            PositionTo = positionTo;
+        }
+    }
+}
---
+++ StigespilletTDD.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StigespilletTDD.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
